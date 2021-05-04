591742fe3a42384ed1a0a8106933782b3ff6abcd
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-05-03 21:46:33 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
+
 function App() {
-  return <div className="text-primary">Olá, Brasil</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá, Brasil</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{' '}
+          <a
+            href="https://www.linkedin.com/in/sidney-miranda"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sidney Miranda
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{' '}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
